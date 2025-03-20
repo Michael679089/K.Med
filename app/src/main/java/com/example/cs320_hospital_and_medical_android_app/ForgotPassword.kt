ee86766d66b50f1544bf62f10b79d59a86ddc3b2
JamesBillate <james.billate@ciit.edu.ph>
2025-03-20 11:24:30 +0800

    Fixing XML of Reset Password

--- app/src/main/java/com/example/cs320_hospital_and_medical_android_app/ForgotPassword.kt
+++ app/src/main/java/com/example/cs320_hospital_and_medical_android_app/ForgotPassword.kt
@@ -2,41 +2,75 @@ package com.example.cs320_hospital_and_medical_android_app
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.core.view.isVisible
 
 class ForgotPassword : AppCompatActivity() {
+
+    private lateinit var  userVerify: LinearLayout
+    private lateinit var  securityQuestion: LinearLayout
+    private lateinit var  newPassword: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.forgotpw_email)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.forgot_password)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.emailVerify)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        userVerify = findViewById(R.id.emailVerify)
+        val userVerifyBtn: Button = findViewById(R.id.userVerifyBtn)
+
+        securityQuestion = findViewById(R.id.securityQuestion)
+        val answerVerifyBtn: Button = findViewById(R.id.answerVerifyBtn)
+
+        newPassword = findViewById(R.id.newPassword)
+        val submitBtn: Button = findViewById(R.id.submitBtn)
+
+        userVerifyBtn.setOnClickListener(){
+            changeLayout(securityQuestion)
         }
 
-        val emailVerifyBtn: Button = findViewById(R.id.verifyBtn_1)
+        answerVerifyBtn.setOnClickListener() {
+            changeLayout(newPassword)
+        }
 
-        emailVerifyBtn.setOnClickListener(){
-            setContentView(R.layout.forgotpw_question)
+        submitBtn.setOnClickListener(){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
-            val questionVerifyBtn: Button = findViewById(R.id.verifyBtn_2)
+    }
 
-            questionVerifyBtn.setOnClickListener(){
-                setContentView(R.layout.forgotpw_password)
+    private fun changeLayout(stepLayout: LinearLayout) {
+        userVerify.visibility = View.GONE
+        securityQuestion.visibility = View.GONE
+        newPassword.visibility = View.GONE
+        stepLayout.visibility = View.VISIBLE
+    }
 
-                val submitBtn: Button = findViewById(R.id.submitBtn)
+    override fun onBackPressed() {
+        when {
+            securityQuestion.isVisible -> {
+                changeLayout(userVerify)
 
-                submitBtn.setOnClickListener(){
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                }
             }
+            newPassword.isVisible -> {
+                changeLayout(securityQuestion)
+            }
+            else -> super.onBackPressed()
         }
-
     }
 }
