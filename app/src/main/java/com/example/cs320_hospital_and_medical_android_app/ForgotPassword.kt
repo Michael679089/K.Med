af23d5bf66eb8e74c24029a369abe41541897cc3
JamesBillate <james.billate@ciit.edu.ph>
2025-03-25 17:14:03 +0800

    Patient Registration XML

--- app/src/main/java/com/example/cs320_hospital_and_medical_android_app/ForgotPassword.kt
+++ app/src/main/java/com/example/cs320_hospital_and_medical_android_app/ForgotPassword.kt
@@ -26,11 +26,6 @@ class ForgotPassword : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.forgot_password)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.emailVerify)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
 
         userVerify = findViewById(R.id.emailVerify)
         val userVerifyBtn: Button = findViewById(R.id.userVerifyBtn)
